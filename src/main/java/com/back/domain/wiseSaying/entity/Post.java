ae98ce5d0d48460a3d3f902291cac1183facedb6
perandowind <perandowind@gmail.com>
2026-02-13 14:49:07 +0900

    8

--- src/main/java/com/back/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/back/domain/wiseSaying/entity/Post.java
@@ -3,15 +3,16 @@ package com.back.domain.wiseSaying.entity;
 import jakarta.persistence.*;
 
 @Entity
-public class WiseSaying {
+public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(length = 100 , nullable = false)
-    private String content;
+    private String title;
 
-    @Column(length = 50 , nullable = false)
-    private String author;
+    /**기본값은 varchar() 타입, 근데 내가 직접 타입 TEXT로 지정함*/
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
 
 }
