32f8a9529b304a5488210f7949f29b3c4b5f5411
perandowind <perandowind@gmail.com>
2026-02-23 12:09:06 +0900

    22, 23

--- src/main/java/com/back/domain/post/service/PostService.java
+++ src/main/java/com/back/domain/post/service/PostService.java
@@ -2,18 +2,24 @@ package com.back.domain.post.service;
 
 import com.back.domain.post.entity.Post;
 import com.back.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 /**내가 만든 스프링을 빈에 등록하는 방법: Componet 어노테이션 붙이기*/
-@Component
+@Service // @Component랑 같은 기능, 가독성을 위해 Service 사용
+@RequiredArgsConstructor
 public class PostService {
 
-    @Autowired
-    private PostRepository postRepository;
+    /** 객체 주입은 "필드 주입, 생성자 주입(권장)"이 있음 */
+    @Autowired //필드주입
+    private final PostRepository postRepository; // 생성자 주입 (final 과 애너테이션으로)
+
+    // 생성자 주입
+//    public PostService(PostRepository postRepository) {}
 
     public Post write(String title, String content) {
 
