c3b6ea4d91dc7bf251b28de43f43fe09e3275f56
perandowind <perandowind@gmail.com>
2026-02-23 14:41:13 +0900

    29

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -1,48 +1,50 @@
 package com.back.global;
 
-import com.back.domain.post.repository.PostRepository;
 import com.back.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BaseInitData {
 
     @Autowired
-    private PostRepository postRepository;
+    @Lazy
+    private BaseInitData self;
 
-    @Autowired
-    private PostService postService;
+    private final PostService postService;
 
     @Bean
     ApplicationRunner initDataRunner() {
         return args -> {
-            work1();
-            work2();
+            self.work1();
+            self.work2();
         };
     }
 
+    @Transactional
     void work1() {
 
-        try{
-        if(postRepository.count() > 0) {
+        if (postService.count() > 0) {
             return;
         }
 
         // 어떤 기능을 테스트 데이터가 2개인 것을 가정하고 개발
         postService.write("제목1", "내용1");
 
-        if(true){
-            throw new RuntimeException("테스트 예외");
-        }
+//        if (true) {
+//            throw new RuntimeException("테스트 예외");
+//        }
 
         postService.write("제목2", "내용2");
-
-    }catch(Exception e){
-            System.out.println("예외 발생 : " + e.getMessage());}
     }
+
 
     void work2() {
         postService.findById(1);
