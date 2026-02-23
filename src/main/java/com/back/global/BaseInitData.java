821465d5de8c2f77ef0f1fedf8c0808fb6ea55d6
perandowind <perandowind@gmail.com>
2026-02-23 12:29:05 +0900

    24

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -26,13 +26,22 @@ public class BaseInitData {
 
     void work1() {
 
+        try{
         if(postRepository.count() > 0) {
             return;
         }
 
+        // 어떤 기능을 테스트 데이터가 2개인 것을 가정하고 개발
         postService.write("제목1", "내용1");
+
+        if(true){
+            throw new RuntimeException("테스트 예외");
+        }
+
         postService.write("제목2", "내용2");
 
+    }catch(Exception e){
+            System.out.println("예외 발생 : " + e.getMessage());}
     }
 
     void work2() {
